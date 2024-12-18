b0956a8e4b82b8b50ac6d3fa83af1bae32c774ce
shashankreddy28 <qn1833ep@go.minnstate.edu>
2024-12-17 23:46:57 -0600

    sample scheduling web app

---
+++ my-calendar-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h2>Welcome to My Calendar App</h2>
+    </div>
+  );
+}
+
+export default App;
